3487df5895542f6a39a86f29156bf4a17e9e63e9
Guruprasad Das <guruprasaddas@Guruprasads-MacBook-Air.local>
2026-04-14 03:15:49 +0530

    YAML file updated

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -1,12 +1,12 @@
-package runners;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-
-@CucumberOptions(
-        features = "@target/failed.txt",
-        glue = {"stepdefs", "hooks"},
-        monochrome = true
-)
-public class FailedRunner extends AbstractTestNGCucumberTests {
-}
+//package runners;
+//
+//import io.cucumber.testng.AbstractTestNGCucumberTests;
+//import io.cucumber.testng.CucumberOptions;
+//
+//@CucumberOptions(
+//        features = "@target/failed.txt",
+//        glue = {"stepdefs", "hooks"},
+//        monochrome = true
+//)
+//public class FailedRunner extends AbstractTestNGCucumberTests {
+//}
